bc6abb81d09a95c3fe825cbdcd6dc2dcbe6505bc
kimdongss <imim20012@gmail.com>
2025-02-05 16:52:25 +0900

    MemberFormDto.java user 기본값 설정

--- student_backend/src/main/java/com/javalab/student/dto/MemberFormDto.java
+++ student_backend/src/main/java/com/javalab/student/dto/MemberFormDto.java
@@ -60,6 +60,7 @@ public class MemberFormDto {
     // 사용자 포인트 (기본값: 0)
     private int points = 0;
 
+    @Builder.Default
     private Role role = Role.USER; // 기본값 USER로 설정  -> 이유 : 권한 없을경우 로그인 안됨
 
     /**
