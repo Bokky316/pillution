f3923a8368af6a311807b869027cbf3740a79c3f
Bokky <Bokky316@gmail.com>
2025-04-05 21:02:21 +0900

    vite.config 수정

--- student_frontend/vite.config.js
+++ student_frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // 백엔드 서버 주소
+        target: 'http://43.202.198.161', // 백엔드 서버 주소
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
